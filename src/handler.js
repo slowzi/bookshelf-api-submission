e89c85b92c99a9c8131b10b8f8e2200f5dc6a821
Dimas <dms.riskidee@gmail.com>
2022-06-03 22:03:30 +0700

    adding get book handler with requirement

--- src/handler.js
+++ src/handler.js
@@ -5,18 +5,6 @@ const books = require("./books");
 const addBook = (request, h) => {
     const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
 
-    const id = nanoid(16);
-    const finished = pageCount === readPage;
-    const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
-
-    const newBook = {
-        id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
-    };
-
-    books.push(newBook);
-    const isSuccess = books.filter((book) => book.id === id).length > 0;
-
     if (name === undefined) {
         const response = h.response({
             status: 'fail',
@@ -34,6 +22,20 @@ const addBook = (request, h) => {
         response.code(400);
         return response;
     }
+
+    const id = nanoid(16);
+    const finished = pageCount === readPage;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newBook = {
+        id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, insertedAt, updatedAt
+    };
+
+    books.push(newBook);
+    const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+    
 
     if (isSuccess) {
         const response = h.response({
@@ -53,9 +55,107 @@ const addBook = (request, h) => {
     });
     response.code(500);
     return response;
-    
+};
+
+const getAllBooks = (request, h) => {
+    const {name, reading, finished} = request.query;
+    const isTrue = !name && ! reading && !finished;
+
+    if (isTrue) {
+        const response = h.response({
+            status: 'success',
+            data: {
+                books: books.map((book) => ({
+                    id: book.id,
+                    name: book.name,
+                    publisher: book.publisher,
+                })),
+            },  
+        });
+        response.code(200);
+        return response;
+    }
+
+    if (name !== undefined) {
+        const bookName = books.filter((book) => book.name.toLowerCase()
+        .includes(name.toLowerCase()) 
+        );
+        
+        const response = h.response({
+            status: 'success',
+            data: {
+                books: bookName.map((book) => ({
+                    id: book.id,
+                    name: book.name,
+                    publisher: book.publisher,
+                })),
+            },  
+        });
+        response.code(200);
+        return response;
+    }
+
+    if (reading !== undefined) {
+        const bookReading = books.filter((book) => Number(book.reading) === Number(reading));
+        
+        const response = h.response({
+            status: 'success',
+            data: {
+                books: bookReading.map((book) => ({
+                    id: book.id,
+                    name: book.name,
+                    publisher: book.publisher,
+                })),
+            },  
+        });
+        response.code(200);
+        return response;
+    }
+
+    if (finished !== undefined) {
+        const bookFinished = books.filter((book) => Number(book.finished) === Number(finished));
+        
+        const response = h.response({
+            status: 'success',
+            data: {
+                books: bookFinished.map((book) => ({
+                    id: book.id,
+                    name: book.name,
+                    publisher: book.publisher,
+                })),
+            },  
+        });
+        response.code(200);
+        return response;
+    }
+};
+
+const getBookById = (request, h) => {
+    const { id } = request.params;
+   
+    const book = books.filter((b) => b.id === id)[0];
+   
+    if (book !== undefined) {
+      const response = h.response ({
+        status: 'success',
+        data: {
+          book,
+        },
+      });
+      response.code(200);
+      return response;
+    }
+
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
 };
 
 module.exports = { 
-  addBook
+  addBook,
+  getAllBooks,
+  getBookById,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
-const { addBook } = require("./handler");
+const { addBook, getAllBooks, getBookById } = require("./handler");
 
 const routes = [
     {
       method: 'POST',
       path: '/books',
       handler: addBook,
+    },
+    {
+      method: 'GET',
+      path: '/books',
+      handler: getAllBooks,
+    },
+    {
+      method: 'GET',
+      path: '/books/{id}',
+      handler: getBookById,
     }
 ];
 
